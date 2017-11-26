ed297d8b4806c30dbf06558e64f95f2e64b1b52d
Glenn <glenn.bickel@gmail.com>
2017-11-26 14:02:09 +0100

    MainView and OfferDetails

--- app/src/main/java/com/example/stealdeal/stealdeal/MainActivity.java
+++ app/src/main/java/com/example/stealdeal/stealdeal/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.stealdeal.stealdeal;
 
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private SearchView searchView;
+    public static ArrayList<Offer> offerList;
+    private MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainActivity = this;
+        listView = findViewById(R.id.mainScreenListView);
+        searchView = findViewById(R.id.mainScreenSearchView);
+
+        //TODO creat real Offers
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        offerList = new ArrayList<Offer>();
+        for(int i = 0; i <= values.length - 1; i++){
+            offerList.add(new Offer(i, values[i], new Location(""), "dummy"));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+                Intent intent = new Intent(mainActivity, OfferDetails.class);
+                //intent.putExtra("Offer", offerList.get(itemPosition));
+                intent.putExtra("offerID", itemPosition);
+                startActivity(intent);
+            }
+        });
     }
 }
